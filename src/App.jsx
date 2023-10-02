8a47e3055e31f1c5afc1f9cba1043d6e5a73e12c
YggdrasilJL <yggdrasiljl@gmail.com>
2023-10-02 20:07:09 -0300

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Dashboard from "./components/Dashboard";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Lesson from "./components/Lesson";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
---
+++ src/components/Lesson.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Lesson = () => {
+  return <div>Lesson</div>;
+};
+
+export default Lesson;
