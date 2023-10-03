cad8a6b506d760c53e186edb4322b464ab087e15
skyler <skyler@skylers-mbp.local>
2023-10-03 20:30:55 -0300

    basic seed structure added with tentative user model

---
+++ server/models/User.js
@@ -0,0 +1,25 @@
+const {Schema, model} = require("mongoose")
+const bcrypt = require("bcrypt")
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+  },
+})
+
+const User = model("User", userSchema)
+
+module.exports = User
