a50dc5742eaf70090d2e0690d68bd765dc6c44ec
YggdrasilJL <yggdrasiljl@gmail.com>
2023-10-04 16:37:31 -0300

    add one lesson

---
+++ client/src/lessons.js
@@ -0,0 +1,20 @@
+const lessons = [
+  {
+    name: 'Lesson 1',
+    url: '/lesson1',
+    code: `// make the console output 'Fluffy'
+
+var shelter = {
+  dogs: ["Mackie", "Bernice", "Cookie Monster", "Spot"],
+  cats: ["Astrid", "Lulu", "Fluffy", "Mouser"]
+};
+
+var chosenPet = function () {
+  // code here
+};
+
+console.log(chosenPet());`,
+  },
+];
+
+export default lessons;
