e41b1ee9a68ad14189ea73c890b02ec09ea8afe9
YggdrasilJL <yggdrasiljl@gmail.com>
2023-10-08 17:25:30 -0300

    add some styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors';
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      // cyberpunk color palette
+      cyber: {
+        black: '#000',
+        yellow: '#FEE801',
+        darkYellow: '#969A27',
+        blue: '#4CC9F0',
+        pink: '#FF00F2',
+      },
+      opacityBlack: 'rgba(0,0,0,0.4)',
+      // gives us all default tailwind colors
+      ...colors,
+    },
   },
   plugins: [],
 };
