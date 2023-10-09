e54774389d98a37618fb2e714e18d407c104bd1a
skyler <skyler@skylers-MacBook-Pro.local>
2023-10-09 12:11:38 -0300

    main pull update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,66 +1,66 @@
 // components
-import Dashboard from './components/Dashboard';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Home from './components/Home';
-import Lesson1 from './components/Lessons/Lesson1';
-import Lessons from './components/Lessons/Lessons';
-import Lesson2 from './components/Lessons/Lesson2';
-import Register from './components/Register';
-import LoginButton from "./components/LoginButton";
-import LogoutButton from "./components/LogoutButton";
-import Profile from "./components/Profile";
-import UserMessages from "./components/UserMessages";
-import Login from './components/Login';
-import Profile from './components/Profile';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Dashboard from "./components/Dashboard"
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Home from "./components/Home"
+import Lesson1 from "./components/Lessons/Lesson1"
+import Lessons from "./components/Lessons/Lessons"
+import Lesson2 from "./components/Lessons/Lesson2"
+import Register from "./components/Register"
+import LoginButton from "./components/LoginButton"
+import LogoutButton from "./components/LogoutButton"
+import Profile from "./components/Profile"
+import UserMessages from "./components/UserMessages"
+import Login from "./components/Login"
+
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 
 const routes = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Home />,
   },
   {
-    path: '/lessons',
+    path: "/lessons",
     element: <Lessons />,
     children: [
       {
-        path: 'lesson1',
+        path: "lesson1",
         element: <Lesson1 />,
       },
       {
-        path: 'lesson2',
+        path: "lesson2",
         element: <Lesson2 />,
       },
     ],
   },
   {
-    path: '/dashboard',
+    path: "/dashboard",
     element: <Dashboard />,
   },
   {
-    path: '/register',
+    path: "/register",
     element: <Register />,
   },
   {
-    path: '/profile',
+    path: "/profile",
     element: <Profile />,
   },
   {
-    path: '/login',
+    path: "/login",
     element: <Login />,
   },
   {},
-]);
+])
 
 const App = () => {
   return (
     <main>
-      <Profile/>
-      <Dashboard/>
+      <Profile />
+      <Dashboard />
       <RouterProvider router={routes} fallbackElement={<Home />} />
     </main>
-  );
-};
+  )
+}
 
-export default App;
+export default App
