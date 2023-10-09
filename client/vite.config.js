2fc6dc92500363bfa43ec94baa133bccf918551d
YggdrasilJL <yggdrasiljl@gmail.com>
2023-10-09 20:49:24 -0300

    edit config, add vite and dotenv to deps

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
 dotenv.config();
 
 export default defineConfig(({ mode }) => {
