6ee443dcfb949bdba045a10d8aca40e07dc0d807
Coral <cnight18@hotmail.com>
2023-10-12 19:12:42 -0300

    updated home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const homeStyle = {
+    background: 'url("/images/background.jpg")', 
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minHeight: '100vh', // full screen
+  };
+
+  return (
+    <div style={homeStyle}>
+      <main className="bg-opacity-80 p-4">
+        {/* text here if we want to add anything  */}
+      </main>
+    </div>
+  );
 };
 
 export default Home;
