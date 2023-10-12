c227f3db758fd8a0ee54899e576641674fc652c9
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-10-12 20:16:30 -0300

    made changes for the auth0 accomodation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -59,12 +59,33 @@ const routes = createBrowserRouter([
   },
 ]);
 
-const App = () => {
-  return (
-    <main>
-      <RouterProvider router={routes} fallbackElement={<Home />} />
-    </main>
-  );
-};
+function App() {
+  const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
+    useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Oops... {error.message}</div>;
+  }
+
+  if (isAuthenticated) {
+    return (
+      <div>
+        Hello {user.name}{' '}
+        <main>
+          <RouterProvider router={routes} />
+        </main>
+
+        <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+          Log out
+        </button>
+      </div>
+    );
+  } else {
+    return <button onClick={() => loginWithRedirect()}>Log in</button>;
+  }
+}
 
 export default App;
