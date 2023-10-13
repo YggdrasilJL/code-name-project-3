677839801313ed319384d4abf8ee13c255732555
skyler <skyler@skylers-MacBook-Pro.local>
2023-10-13 19:47:36 -0300

    authMiddleware added with server.js configuration

--- server/server.js
+++ server/server.js
@@ -1,51 +1,52 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+const express = require("express")
+const {ApolloServer} = require("apollo-server-express")
+const path = require("path")
 //const { verifyToken } = require('./utils/authMiddleware')
-const Stripe = require('stripe');
-const cors = require('cors');
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const Stripe = require("stripe")
+const cors = require("cors")
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-require('dotenv').config();
+require("dotenv").config()
 
-const { typeDefs, resolvers } = require('./schemas/');
-const db = require('./config/connection');
+const {typeDefs, resolvers} = require("./schemas/")
+const db = require("./config/connection")
+const authMiddleware = require("./utils/authMiddleware")
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express()
+const PORT = process.env.PORT || 3001
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  //context: verifyToken
-});
+  context: authMiddleware,
+})
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors());
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use(cors())
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")))
 }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client//dist/index.html'));
-});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client//dist/index.html"))
+})
 
-app.get('*', function (req, res) {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-});
+app.get("*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+})
 
 const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
+  await server.start()
+  server.applyMiddleware({app})
 
-  db.once('open', () => {
+  db.once("open", () => {
     app.listen(PORT, () => {
-      console.log(`API server running on port ${PORT}!`);
-      console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`);
-    });
-  });
-};
+      console.log(`API server running on port ${PORT}!`)
+      console.log(`🌍 Now listening on localhost:${PORT}${server.graphqlPath}`)
+    })
+  })
+}
 
-startApolloServer(typeDefs, resolvers);
+startApolloServer(typeDefs, resolvers)
--- server/utils/authMiddleware.js
+++ server/utils/authMiddleware.js
@@ -1,40 +1,76 @@
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
+const {GraphQLError} = require("graphql")
+const jwt = require("jsonwebtoken")
+// const jwksClient = require('jwks-rsa');
 
-const client = jwksClient({
-    jwksUri: `https://dev-2kax28qvyzlsa7s0.us.auth0.com/.well-known/jwks.json`
-});
-
-function getKey(header, cb) {
-    client.getSigningKey(header.kid, function (err, key) {
-        var signingKey = key.publicKey || key.rsaPublicKey;
-        cb(null, signingKey);
-    });
-}
-
-const options = {
-    audience: 'qGqfNVoVpHZlptcz8qAgV5k6H7qCShVU',
-    issuer: `https://dev-2kax28qvyzlsa7s0.us.auth0.com/`,
-    algorithms: ['RS256']
-};
+const secret = "mysecretssshhhhhhh"
+const expiration = "2h"
 
 module.exports = {
-    verifyToken: ({ req }) => {
-        const token = req.headers.authorization;
-        const user = new Promise((resolve, reject) => {
-          jwt.verify(token, getKey, options, (err, decoded) => {
-            if(err) {
-              return reject(err);
-            }
-            resolve(decoded.email);
-          });
-        });
-    
-        return {
-          user
-        };
+  AuthenticationError: new GraphQLError("Could not authenticate user.", {
+    extensions: {
+      code: "UNAUTHENTICATED",
     },
-    signToken: () => {
+  }),
+  authMiddleware: function ({req}) {
+    let token = req.body.token || req.query.token || req.headers.authorization
 
-    },
+    if (req.headers.authorization) {
+      token = token.split(" ").pop().trim()
+    }
+
+    if (!token) {
+      return req
+    }
+
+    try {
+      const {data} = jwt.verify(token, secret, {maxAge: expiration})
+      req.user = data
+    } catch {
+      console.log("Invalid token")
+    }
+
+    return req
+  },
+  signToken: function ({email, username, _id}) {
+    const payload = {email, username, _id}
+    return jwt.sign({data: payload}, secret, {expiresIn: expiration})
+  },
 }
+
+// const client = jwksClient({
+//     jwksUri: `https://dev-2kax28qvyzlsa7s0.us.auth0.com/.well-known/jwks.json`
+// });
+
+// function getKey(header, cb) {
+//     client.getSigningKey(header.kid, function (err, key) {
+//         var signingKey = key.publicKey || key.rsaPublicKey;
+//         cb(null, signingKey);
+//     });
+// }
+
+// const options = {
+//     audience: 'qGqfNVoVpHZlptcz8qAgV5k6H7qCShVU',
+//     issuer: `https://dev-2kax28qvyzlsa7s0.us.auth0.com/`,
+//     algorithms: ['RS256']
+// };
+
+// module.exports = {
+//     verifyToken: ({ req }) => {
+//         const token = req.headers.authorization;
+//         const user = new Promise((resolve, reject) => {
+//           jwt.verify(token, getKey, options, (err, decoded) => {
+//             if(err) {
+//               return reject(err);
+//             }
+//             resolve(decoded.email);
+//           });
+//         });
+
+//         return {
+//           user
+//         };
+//     },
+//     signToken: () => {
+
+//     },
+// }
