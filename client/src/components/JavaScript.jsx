62c80ed9b45f34c22e093d3f9a760870c3d81ab9
Coral <cnight18@hotmail.com>
2023-10-14 21:59:22 -0300

    starter lesson pages

---
+++ client/src/components/JavaScript.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ParticleEffect from './ParticleEffect';
+
+const JavaScript = () => {
+  return (
+    <CommonLayout>
+      <ParticleEffect />
+      <div>JavaScript</div>
+    </CommonLayout>
+  );
+}
+
+export default JavaScript;
