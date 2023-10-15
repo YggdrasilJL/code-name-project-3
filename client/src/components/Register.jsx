075affcbfce71527b28250d30c05cdd0cf04a4c3
aedelfrid <almck51@gmail.com>
2023-10-15 17:51:22 -0300

    fixed register and login

--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -27,9 +27,19 @@ const Register = () => {
     event.preventDefault();
     console.log(formState);
 
+    if (formState.password !== formState.confirmPassword) {
+      return console.error('Passwords do not match');
+    }
+
     try {
+      const { username, email, password } = formState;
       const { data } = await addUser({
-        variables: { ...formState },
+        variables: { 
+          userData: { 
+            username, 
+            email, 
+            password 
+          }},
       });
 
       Auth.login(data.addUser.token);
@@ -114,12 +124,12 @@ const Register = () => {
                   </label>
                   <input
                     type="password"
-                    name="confirm-password"
-                    id="confirm-password"
+                    name="confirmPassword"
+                    id="confirmPassword"
                     placeholder="••••••••"
                     className=" form-input bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required
-                    value={formState.password}
+                    value={formState.confirmPassword}
                     onChange={handleChange}
                   />
                 </div>
