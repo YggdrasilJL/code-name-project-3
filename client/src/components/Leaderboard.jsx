d41c0b9772097397c7fa33a353466ca71daec9e4
skyler <skyler@skylers-MacBook-Pro.local>
2023-10-16 16:57:37 -0300

    leaderboard finalized

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -8,37 +8,67 @@ const TABLE_ROWS = [
     Ranking: 1,
     UserLogo: "../../public/images/mockpfp.png",
     Username: "NexusNightmare",
-    Score: "460",
+    Score: "500",
   },
   {
     Ranking: 2,
-    UserLogo: "../../public/images/mockpfp.png",
+    UserLogo: "../../public/images/Avatar1.png",
     Username: "VoidVoyager",
-    Score: "390",
+    Score: "480",
   },
   {
     Ranking: 3,
-    UserLogo: "../../public/images/mockpfp.png",
+    UserLogo: "../../public/images/Avatar2.png",
     Username: "SaurahbTheHeartthrob",
-    Score: "380",
+    Score: "450",
   },
   {
     Ranking: 4,
-    UserLogo: "../../public/images/mockpfp.png",
+    UserLogo: "../../public/images/Avatar3.png",
     Username: "ChronoCrusader",
-    Score: "350",
+    Score: "400",
   },
   {
     Ranking: 5,
-    UserLogo: "../../public/images/mockpfp.png",
+    UserLogo: "../../public/images/Avatar4.png",
     Username: "VortexVandal",
+    Score: "390",
+  },
+  {
+    Ranking: 6,
+    UserLogo: "../../public/images/Avatar5.png",
+    Username: "NinjaGnome",
+    Score: "350",
+  },
+  {
+    Ranking: 7,
+    UserLogo: "../../public/images/Avatar6.png",
+    Username: "PixelProwler23",
+    Score: "320",
+  },
+  {
+    Ranking: 8,
+    UserLogo: "../../public/images/Avatar7.png",
+    Username: "QuantumKraken",
+    Score: "200",
+  },
+  {
+    Ranking: 9,
+    UserLogo: "../../public/images/Avatar8.png",
+    Username: "HelixHunter",
     Score: "190",
+  },
+  {
+    Ranking: 10,
+    UserLogo: "../../public/images/Avatar9.png",
+    Username: "CtrlAltDefeat",
+    Score: "50",
   },
 ]
 
 export default function Leaderboard() {
   return (
-    <div class=" flex flex-col w-full items-center justify-center">
+    <div class=" flex flex-col w-full items-center justify-center mb-5">
       <div class="bg-black mb-2 rounded-lg border-solid border-2 border-cyber-yellow flex justify-center">
         <h1 class="text-white m-1">Leaderboard</h1>
       </div>
