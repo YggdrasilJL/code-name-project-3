76dd6811850f91b44dc397d7d4b5dd42b1f1aea1
aedelfrid <almck51@gmail.com>
2023-10-17 21:05:11 -0300

    revert to this commit, i hope?

--- client/utils/auth.js
+++ client/utils/auth.js
@@ -29,7 +29,7 @@ class AuthService {
 
   login(idToken) {
     localStorage.setItem('id_token', idToken);
-    window.location.assign('/me');
+    window.location.assign('/user/me');
   }
 
   logout() {
